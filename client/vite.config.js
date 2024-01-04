af6d52d9e95b473d74134bd30f7c2355c02b62fb
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-01-04 13:36:47 +0000

    update vite config js file

---
+++ client/vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__root, 'index.html'),
+        calendar: resolve(__calendar, 'calendar/index.html'),
+        collection: resolve(__collection, 'collection/index.html'),
+        Marketplace: resolve(__Marketplace, 'Marketplace/index.html'),
+        firstplayer: resolve(__firstplayer, 'firstplayer/index.html'),
+      },
+    },
+  },
+});
